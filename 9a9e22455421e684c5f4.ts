f2098baa4a0b3d4519a259409610c50e31a39cd4
Artyom445work <Artyom445work@users.noreply.github.com>
2025-01-31 17:20:10 +0000

    deploy: 5085aa44962e755c3f25722ff361d5a0b6a42d75

--- 9a9e22455421e684c5f4.ts
+++ 9a9e22455421e684c5f4.ts
@@ -125,7 +125,7 @@ router.addRoute({
     showWeather(weather);
     const coords = await getCityCoords(city, openweathermapApiKey);
     createMap(coords);
-    let citiesList = loadList();
+    const citiesList = loadList();
     if (!citiesList.includes(city)) {
       citiesList.push(city);
       if (citiesList.length > 10) citiesList.shift();
